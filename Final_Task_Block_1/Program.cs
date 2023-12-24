74ad91b187b668671ee5067038138df38f76e3a1
Your Name <youremail@domain.com>
2023-12-24 12:11:36 +0500

    Добавление файла в репозиторий

---
+++ Final_Task_Block_1/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] unsortedArray = new string[4] {"Hello", "2", "world", ":-)"};
+string[] sortedArray = new string[unsortedArray.Length];
+
+void ArraySorting(string[] unsortedArray, string[] sortedArray)
+{
+    int count = 0;
+    for (int i = 0; i < unsortedArray.Length; i++)
+    {
+        if (unsortedArray[i].Length <= 3)
+        {
+            sortedArray[count] = unsortedArray[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ArraySorting(unsortedArray, sortedArray);
+PrintArray(sortedArray);
